e3f7d3db7c20e1465c22b7ee55a0405e2b7b0989
Barrutess <duarte.v.barros@gmail.com>
2023-01-23 10:27:00 +0000

    Atualizações

--- estag11.php
+++ estag11.php
@@ -89,42 +89,44 @@
         <?php
 
             $curl = curl_init();
-            if($b != NULL)
-            {
-                curl_setopt_array($curl, 
+        if ($b != NULL) {
+            curl_setopt_array(
+                $curl,
                 [
-                CURLOPT_URL => "https://mx.multimac.pt/mxv5/api/v1/Case/".$b,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-                CURLOPT_POSTFIELDS => "",
-                CURLOPT_HTTPHEADER => ["X-Api-Key: 4551D74F0502A6409445E49961896B49"],
-                ]);
+                    CURLOPT_URL => "https://mx.multimac.pt/mxv5/api/v1/Case/" . $b,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                    CURLOPT_POSTFIELDS => "",
+                    CURLOPT_HTTPHEADER => ["X-Api-Key: 4551D74F0502A6409445E49961896B49"],
+                ]
+            );
 
-                // Desactiva o certificado SSL
-                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                $response = curl_exec($curl);
-                $err = curl_error($curl);
-                curl_close($curl);
+            // Desactiva o certificado SSL
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
 
-                    if ($err) 
-                    {
-                    echo "cURL Error #:" . $err;
-                    } 
-                    else 
-                    {
-                    $array = json_decode($response,true);
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } 
+            else 
+            {
+                $array = json_decode($response, true);
 
-                    foreach($array as $key => $value) 
-                    {
-                        $a[$key] = $value;
-                        echo $key . " => " . $value . "<br><-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->-<-->--<--><br>";                    }
-                    }
+                foreach ($array as $key => $value) 
+                {
+                    $a[$key] = $value;
+                    echo $key . " => " . $value . "<br><-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->--<-->-<-->--<--><br>";
+                    error_reporting(E_ALL & ~E_WARNING);
+                }
             }
+        }
             else{}
 
         ?>
