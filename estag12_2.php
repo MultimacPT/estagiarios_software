ccc1a5f5995a781d1e6373b34b095b7d63dcc080
Catita13 <joaocatita05@gmail.com>
2023-01-27 14:10:10 +0000

    novo

--- estag12_2.php
+++ estag12_2.php
@@ -174,6 +174,8 @@ foreach ($array as $key => $value) {
 
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 
+    <link href="estag12.php" rel="import" />
+
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 
     <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
