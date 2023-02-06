8ccbc4ea90b7c0b61cfd524ad6ca956895adf9c6
danielrodrigues16 <dr8470882@gmail.com>
2023-02-06 17:35:15 +0000

    desenvolvimento do projeto garantia

--- estag12.php
+++ estag12.php
@@ -1,87 +1,87 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Formulário</title>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<html lang="en">
 
-        <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<head>
 
-        <script>
-            var _hmt = _hmt || [];
-            (function() {
-            var hm = document.createElement("script");
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(hm, s);
-            })();
-        </script>
-    </head>
+    <meta charset="UTF-8">
 
-    <body>
-        <div data-role="page">
-            <div data-role="header">
-                <h3>Formulário Básico</h3>
-            </div>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-            <div data-role="main" class="ui-content">
-                <form method="post" action="demoform.asp">
-                    <fieldset data-role="controlgroup">
-                        <legend>Suas informações:</legend>
-                        <label for="nome">Nome:</label>
-                        <input type="text" name="nome" id="nome" placeholder="Dani ">
-                        <br>
-                        <label for="bday">Aniversário:</label>
-                        <input type="date" name="bday" id="bday">
-                        <br>       
-                        <label for="email">E-mail:</label>
-                        <input type="email" name="email" id="email" placeholder="Seu email...">
-                        <br>
-                        <label for="male">Masculino</label>
-                        <input type="radio" name="gender" id="male" value="male">
-                        <label for="female">Feminino</label>
-                        <input type="radio" name="gender" id="female" value="female">
-                    </fieldset>
-                    
-                    <fieldset class="ui-field-contain">
-                    <legend>Data:</legend>
-                        <label for="data">Data de hoje:</label>
-                        <input type="date" name="data" id="data">
-                        <br>
-                        <label for="day">Dia da semana</label>
-                        <select name="day" id="day" data-native-menu="false">
-                        <option value="mon">Segunda</option>
-                        <option value="tue">Terça</option>
-                        <option value="wed">Quarta</option>
-                        <option value="thu">Quinta</option>
-                        <option value="fri">Sexta</option>
-                        <option value="sat">Sábado</option>
-                        <option value="sun">Domingo</option>
-                        </select>
-                    </fieldset>
-                    <fieldset data-role="controlgroup">
-                        <legend>Tem conhecimento em qual destas linguagens:</legend>
-                        <label for="c">C</label>
-                        <input type="checkbox" name="ling" id="c" value="c">
-                        <label for="cs">C#</label>
-                        <input type="checkbox" name="ling" id="cs" value="cs">
-                        <label for="j">Java</label>
-                        <input type="checkbox" name="j" id="j" value="j">
-                        <label for="js">Java Script</label>
-                        <input type="checkbox" name="ling" id="js" value="js">
-                        <label for="outros">Outras</label>
-                        <input type="checkbox" name="ling" id="outros" value="outros">	
-                    </fieldset>
-                    <input type="submit" data-inline="true" value="Submeter">
-                </form>
-            </div>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-            <div data-role="footer">
-                <p>Feito por:<br>aka papi</p>
-            </div>
-        </div> 
+    <title>Formulário</title>
+
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+
+
+
+    <!--script>
+
+  function idSearch(){
+
+    var URLid = document.getElementById('ID').value;
+
+    window.location.href = "estag12_2.php?id=" + URLid;
+
+  }
+
+  </script-->
+
+
+
+</head>
+
+    <body >
+
+
+
+    <div data-role="page">
+
+    <div data-role="header" data-position="fixed">
+
+    <h1>MÉTODO DE PESQUISAS</h1>
+
+    </div>
+
+
+
+        <div data-role="main" class="ui-content">
+
+            <!--form method="get" action="estag12_2.php">
+
+                <div data-role="content">
+
+                    <p for="ID">ID: :</p>
+
+                    <input type="text"  id="ID">
+
+                    <input type="submit" data-inline="true" value="Submit"-->
+
+             <!--input type="submit" data-inline="true" value="Submit" id="click" onclick=idSearch()-->
+
+             
+
+            <!--/form-->
+
+            <form action="estag12_2.php" method="post" data-transition="flip">
+
+              <label for="lname" data-mini="true">id:</label>
+
+              <input type="text" id="lname" name="id"><br><br>
+
+              <input type="submit" value="Submit" data-mini="true">
+
+            </form>
+
+        </div>
+
+    </div>
+
     </body>
-    
-    
-</html>
+    </html>
