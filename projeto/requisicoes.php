f20afa1019b678f573dac15f521aed39f87270e4
manuelpereira <manuel.pereiraa2006@gmail.com>
2023-02-08 11:38:23 +0000

    ultimo

--- projeto/requisicoes.php
+++ projeto/requisicoes.php
@@ -1,11 +1,41 @@
 <?php
-//print_r($_GET);
 
-$codmaq=331581;
+$i = 0;
+$x = 0;
+$getcodmaq='331581';//desenvolvimento
+$getnumeroit='3563';//desenvolvimento
+//$getcodmaq=$_GET['getcodmaq'];//producao
+//$getnumeroit=$_GET['getnumeroit'];//producao
+
+$curlCOD = curl_init();
+curl_setopt_array($curlCOD, [
+    CURLOPT_URL => "https://mx.multimac.pt/mxv5/api/v1/Equipamentos?maxSize=25&offset=0&orderBy=createdAt&order=desc&where%5B0%5D%5Btype%5D=startsWith&where%5B0%5D%5Battribute%5D=name&where%5B0%5D%5Bvalue%5D=".$getcodmaq,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_POSTFIELDS => "",
+    CURLOPT_HTTPHEADER => ["X-Api-Key: 4551D74F0502A6409445E49961896B49"],
+]);
+
+curl_setopt($curlCOD, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curlCOD, CURLOPT_SSL_VERIFYPEER, false);
+$responseCOD = curl_exec($curlCOD);
+$errCOD = curl_error($curlCOD);
+curl_close($curlCOD);
+
+if($errCOD){
+    echo "cURL Error #:" . $errCOD;
+} 
+else{
+    $arrayCOD = json_decode($responseCOD, true);
+}
 
 $curl = curl_init();
 curl_setopt_array($curl, [
-    CURLOPT_URL => "https://mx.multimac.pt/mxv5/api/v1/Consumiveisvsmodelo?where%5B0%5D%5Btype%5D=equals&where%5B0%5D%5Battribute%5D=modelo&where%5B0%5D%5Bvalue%5D=XC4240",
+    CURLOPT_URL => "https://mx.multimac.pt/mxv5/api/v1/Consumiveisvsmodelo?where%5B0%5D%5Btype%5D=equals&where%5B0%5D%5Battribute%5D=modelo&where%5B0%5D%5Bvalue%5D=".$arrayCOD['list'][0]['modelo'],
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
@@ -26,51 +56,44 @@ if($err){
     echo "cURL Error #:" . $err;
 } 
 else{
-    $array = json_decode($response, true);
+    $arrayMOD = json_decode($response, true);
 }
 
-$list = json_encode($array['list']);
-$list = json_decode($list, true);
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://mx.multimac.pt/mxv5/api/v1/Requisicoes?maxSize=25&offset=0&orderBy=numreq&order=desc&where%5B0%5D%5Btype%5D=equals&where%5B0%5D%5Battribute%5D=equipamentosId&where%5B0%5D%5Bvalue%5D=".$arrayCOD['list'][0]['id'],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_POSTFIELDS => "",
+    CURLOPT_HTTPHEADER => ["X-Api-Key: 4551D74F0502A6409445E49961896B49"],
+]);
 
-$i = 0;
-$x = 0;
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
 
-foreach ($list as $item) {
-    $ID = $item['id'];
+if($err){
+    echo "cURL Error #:" . $err;
+} 
+else{
+    $arrayREQ = json_decode($response, true);
+}
 
-    $curlID = curl_init();
-    curl_setopt_array($curlID, [
-        CURLOPT_URL => "https://mx.multimac.pt/mxv5/api/v1/Consumiveisvsmodelo/" . $ID,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_POSTFIELDS => "",
-        CURLOPT_HTTPHEADER => ["X-Api-Key: 4551D74F0502A6409445E49961896B49"],
-    ]);
-
-    curl_setopt($curlID, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curlID, CURLOPT_SSL_VERIFYPEER, false);
-    $responseID = curl_exec($curlID);
-    $errID = curl_error($curlID);
-    curl_close($curlID);
-
-    if ($errID) {
-        //echo "cURL Error #:" . $errID;
-    } else {
-        $arrayID = json_decode($responseID, true);
+foreach ($arrayREQ['list'] as $k => $v){
+    if ($v['itnum']==$getnumeroit){
+        $displayErro="block";
     }
-
-    $ID_display[$i] = $arrayID['id'];
-    $name_display[$i] = $arrayID['name'];
-    $desc_display[$i] = $arrayID['description'];
-    $mod_display[$i] = $arrayID['modelo'];
-    $prodidInpt[$i]="";
-    $fID[$i] = $arrayID;
-    $i += 1;
+    else{
+        $displayErro="none";
+    }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -83,7 +106,7 @@ foreach ($list as $item) {
 
 <body>
     <?php
-    if (isset($_GET['submit'])) {
+    if (isset($_GET['submit']) and  $displayErro!="block") {
         $quantidade=0;
         $curl = curl_init();
 
@@ -97,8 +120,6 @@ foreach ($list as $item) {
         CURLOPT_SSL_VERIFYPEER=> false,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => "{\"tipo\":\"Peça\",\"assignedUserId\":\"63bbf90a15e21a5df\",\"assignedUserName\":\"mpereira\",\"teamsIds\":[\"63bbf88d567391e32\"],\"teamsNames\":{\"63bbf88d567391e32\":\"Estagiarios\"},
-        \"accountName\":\"Multimac S.A\",\"accountId\":\"60a7c36c4cfecf17b\",\"equipamentosName\":\"00001\",\"equipamentosId\":\"606f2ddc5c6f30909\",\"quantidade\":\"" .$quantidade. "\",\"productName\":\"00B1004115///\",\"productId\":\"62b51d4c77b7ff977\"}",
         CURLOPT_HTTPHEADER => [
             "Accept: application/json, text/javascript, */*; q=0.01",
             "Accept-Encoding: gzip, deflate, br",
@@ -115,16 +136,18 @@ foreach ($list as $item) {
                 
                 $idreq=substr($k, 4);
                 $prodid=substr($k, 4);
+                
                 $quantidade=$v;
 
                 $var="{\"tipo\":\"Peça\",\"assignedUserId\":\"63bbf90a15e21a5df\",\"assignedUserName\":\"mpereira\",\"teamsIds\":[\"63bbf88d567391e32\"],\"teamsNames\":{\"63bbf88d567391e32\":\"Estagiarios\"},
-                \"accountName\":\"Multimac S.A\",\"accountId\":\"60a7c36c4cfecf17b\",\"equipamentosName\":\"00001\",\"equipamentosId\":\"606f2ddc5c6f30909\",\"quantidade\":\"" .$quantidade. "\",\"productId\":\"".$prodid."\"}";
+                \"accountId\":\"".$arrayCOD['list'][0]['accountId']."\",\"itnum\":\"" . $getnumeroit . "\",\"equipamentosId\":\"".$arrayCOD['list'][0]['id']."\",\"quantidade\":\"" .$quantidade. "\",\"productId\":\"".$prodid."\"}";
             
                 curl_setopt($curl, CURLOPT_POSTFIELDS, $var);
                 $response = curl_exec($curl);
                 $err = curl_error($curl);
             }
         }
+
         curl_close($curl);
 
         if ($err) {
@@ -139,10 +162,8 @@ foreach ($list as $item) {
         <header>
             <div class="ui-grid-c" style="padding-right: 20px;">
                 <div class="ui-block-a" style="display:table; margin:0 auto;">
-                    <span style="display:table; margin:0 auto; font-size: 10px;text-align: center;">
-                        <br>
-                        <img src="images/mxtechnovo.png" style="width: 60px">
-                        <br>
+                    <span style="display:table; margin:0 auto; font-size: 10px;text-align: center;"><br>
+                        <img src="images/mxtechnovo.png" style="width: 60px"><br>
                         <font size="1">
                             <a href="#popupDialog" data-rel="popup" data-position-to="window" data-transition="pop"
                                 aria-haspopup="true" aria-owns="popupDialog" aria-expanded="false" class="ui-link">
@@ -162,52 +183,49 @@ foreach ($list as $item) {
                 </div>
 
                 <div class="ui-block-d">
-                    <input type="hidden" name="submit" value="Submeter" />
+                    <input type="hidden" name="getcodmaq" value="<?= $getcodmaq ?>" />
+                    <input type="hidden" name="getnumeroit" value="<?= $getnumeroit ?>" />
                     <button type="submit" form="form1" value="submit" data-mini="true" data-theme="c" class=" ui-btn ui-btn-c ui-shadow ui-corner-all ui-mini">Submeter</button>
+
                 </div>
+
+                <script>
+                    function limparInput(){
+                    window.location.href = 'requisicoes.php';
+                    } 
+                </script>
             </div>
         </header>
 
         <div data-role="page" style="background-color: #C6D0E2; margin-top:70px !important;">
-            <div class="parentx">    
+            <div class="parentx">   
+                <!--a href="#popupBasic" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="pop">Basic Popup</a>
+                <div data-role="popup" id="popupBasic">
+                <p>This is a completely basic popup, no options set.</p>
+                </div-->    
+                <br> 
                 <?php
-                for ($x; $x < $i; $x++) {
-
-                    foreach ($fID[$x] as $key => $value) {
-                        if (!is_string($key)) {
-                            $key = json_encode($key);
-                        }
-                        if (!is_string($value)) {
-                            $value = json_encode($value);
-                        }
-                        if (!is_string($key) and !is_string($value)) {
-                        }
-                        if ($key == "id" or $key == "name" or $key == "description" or $key == "modelo") {
-                        }
-                        if ($key == "productId") {
-                            $prodidInpt[$i]=$value;
-                        }    
-                    }
-
+                foreach($arrayMOD['list'] as $k => $v){ 
+                    $x++;
                     echo "
                     <div class='normn' style='margin-top: -5px; border: 1px solid #ffa50000; background-color: #0000ff00; padding: 0px;'>
                         <div class='ui-grid-solo'>
                             <div class='ui-block-a'>
                                 <div style='margin-left: 5px !important;' data-role='collapsible' data-theme='c' data-content-theme='b' data-mini='true'>
                                     <h6>
-                                        <span style='margin-left: 1.5%'>PEÇA ". $x+1 ." </span>
+                                        <span style='margin-left: 1.5%'>PEÇA ". $x ." </span>
                                         <br><br><br>
                                         <div class='ui-li-count' style='margin-top: -0.5em !important; width: 35% !important; font-size: 11px; right: auto !important; left: 10% !important; background-color:lightgreen; color:black;'>
-                                            Id : ". $ID_display[$x] ." 
+                                            Id : ". $v['id'] ." 
                                         </div>
                                         <div class='ui-li-count' style='margin-top: -0.5em !important; width: 25% !important; font-size: 11px; right: auto !important; left: 49% !important; background-color:lightgreen; color:black;'>
-                                            Nome : ". $name_display[$x] ." 
+                                            Nome : ". $v['name'] ." 
                                         </div>
                                         <div class='ui-li-count' style='margin-top: 1.5em !important; width: 35% !important; font-size: 11px; right: auto !important; left: 10% !important; background-color:lightgreen; color:black;'>
-                                            Peça : ". $desc_display[$x] ." 
+                                            Peça : ". $v['description'] ." 
                                         </div>
                                         <div class='ui-li-count' style='margin-top: 1.5em !important; width: 25% !important; font-size: 11px; right: auto !important; left: 49% !important; background-color:lightgreen; color:black;'>
-                                            Modelo : ". $mod_display[$x] ." 
+                                            Modelo : ". $v['modelo'] ." 
                                         </div>
                                         &nbsp;
                                         <span class='ui-li-count' style='width: 14% !important; margin-top: 1.1em !important;font-size: 12px; right: 8px !important; left: auto !important; background-color:lightgrey; color:black;'>
@@ -217,17 +235,11 @@ foreach ($list as $item) {
                                     <h6 class='ui-collapsible-heading'>
                                         <table>
                                             <tr>
-                                                <td> <input type='text' id='id" . $ID_display[$x] . "' name='nmqt" . $prodidInpt[$i] . "' data-theme='a' data-content='a' placeholder='Quant.'> <td>
-                                                <td> <input type='checkbox' name='nmop" . $ID_display[$x] . "' style='margin: -1.26em 0;'> </td>
+                                                <td> <input type='text' id='id" . $v['productId'] ."' name='nmqt" . $v['productId'] . "' data-theme='a' data-content='a' placeholder='Quant.'> <td>
+                                                <td> <input type='checkbox' name='nmop" . $x . "' style='margin: -1.26em 0;'> </td>
                                             </tr>
                                         </table>
                                     </h6>
-                                    <script>
-                                        function limparInput(){
-                                        var id = document.getElementById('id" . $ID_display[$x] . "').value;
-                                        window.location.href = 'requisicoes.php';
-                                        } 
-                                    </script>
                                 </div>
                             </div>
                         </div>
@@ -236,7 +248,59 @@ foreach ($list as $item) {
                 ?>
                 <br>
             </div>
+            <?php
+            foreach ($arrayREQ['list'] as $k => $v){
+                
+            }
+            echo "
+            <div style='margin-left: 5px !important;' data-role='collapsible' data-theme='f' data-content-theme='b' data-mini='true'>
+                <h6>
+                    <span style='margin-left: 1.5%'>LISTA</span>
+                    <br><br><br>
+                </h6>
+                <h6 class='ui-collapsible-heading'>
+                    <table>
+                        <tr>
+                            <td> 
+                                <div class='ui-li-count' style='margin-top: -0.5em !important; width: 18% !important; font-size: 11px; right: auto !important; left: 1% !important; background-color:lightgrey; color:black;'>Equipamentos</div> 
+                            </td>
+                            <td> 
+                                <div class='ui-li-count' style='margin-top: -0.5em !important; width: 8% !important; font-size: 11px; right: auto !important; left: 24% !important; background-color:lightgrey; color:black;'>ItNum</div> 
+                            </td>
+                            <td> 
+                                <div class='ui-li-count' style='margin-top: -0.5em !important; width: 11% !important; font-size: 11px; right: auto !important; left: 37% !important; background-color:lightgrey; color:black;'>NumReq</div> 
+                            </td>
+                            <td> 
+                                <div class='ui-li-count' style='margin-top: -0.5em !important; width: 5% !important; font-size: 11px; right: auto !important; left: 53% !important; background-color:lightgrey; color:black;'>Tipo</div> 
+                            </td>
+                            <td> 
+                                <div class='ui-li-count' style='margin-top: -0.5em !important; width: 10% !important; font-size: 11px; right: auto !important; left: 63% !important; background-color:lightgrey; color:black;'>Produto</div> 
+                            </td>
+                            <td> 
+                                <div class='ui-li-count' style='margin-top: -0.5em !important; width: 10% !important; font-size: 11px; right: auto !important; left: 78% !important; background-color:lightgrey; color:black;'>Qntd.</div> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                   
+                            </td>
+                        </tr>
+                    </table>
+                    <br>
+                </h6>
+            </div>";
+            ?>
         </div>
     </form>
 </body>
 </html>
+<p>".$v['equipamentosName']."</p> </td>
+                            <td> <p>".$v['numreq']."</p> <td>
+                            <td> <p>".$v['itnum']."</p> <td>
+                            <td> <p>".$v['accountName']."</p> <td>
+                            <td> <p>".$v['numreq']."</p> <td>
+                            <td> <p>".$v['tipo']."</p> <td>
+                            <td> <p>".$v['productName']."</p> <td>
+                            <td> <p>".$v['quantidade']."</p> <td>
+                            <td> <p>".$v['createdAt']."</p> <td>
+                            <td> <p>".$v['createdByName']."</p> <td>
